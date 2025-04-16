4b60d2f8fffa836e6bc83638d27baea5d20a1d92
Rashad Mowatt <98135951+rmowatt21@users.noreply.github.com>
2025-04-15 19:33:14 -0700

    Create WN10-CC-000155.ps1

---
+++ WN10-CC-000155.ps1
@@ -0,0 +1,17 @@
+​To remediate WN10-CC-000155, which mandates that Solicited Remote Assistance must not be allowed, you need to disable the feature that permits users to request remote assistance. 
+This is crucial because allowing solicited remote assistance can expose systems to unauthorized access, posing a significant security risk.​
+
+
+How to remedaite WN10-CC-000155 manually:
+
+1.Open gpedit.msc in the search bar 
+
+2. Computer Configuration > Administrative Templates > System > Remote Assistance
+
+3. right of the pane, double-click on "Configure Solicited Remote Assistance""
+
+4. select disabled 
+
+5. Apply, and reset 
+
+
